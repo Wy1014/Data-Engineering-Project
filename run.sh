2df31fcb1023928ad6795f47cd25afe321b360e2
Wy1014 <49870150+Wy1014@users.noreply.github.com>
2020-12-15 22:46:07 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,18 @@
-cd /Users/mengyu/Desktop/课件/S9/engneering/tp2
-flask run app.py
+#!/bin/bash
+
+gnome-terminal --tab -- bash -c "docker-compose up"
+
+while true
+do
+    wget -q --spider https://localhost:5000
+
+    if [ &? -eq 0 ]; then
+        echo "Online"
+        break
+    else
+        echo "Offline"
+        sleep 5
+    fi
+done
+
+python app.py
